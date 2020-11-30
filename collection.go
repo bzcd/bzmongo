dc61d0ef9e5e4a4510d5894248e3ba91b283a938
joiky <joiky@163.com>
2020-11-30 05:28:08 +0000

    rename find -> query

--- collection.go
+++ collection.go
@@ -11,11 +11,15 @@ type Collection struct {
 	*mongo.Collection
 }
 
-func (c *Collection) FindMany(ctx context.Context, out interface{}, filter interface{}, opts ...*options.FindOptions) error {
-	cur, err := c.Find(ctx, filter, opts...)
+func (c *Collection) Query(ctx context.Context, out interface{}, filter interface{}, opts ...*options.FindOptions) error {
+	cur, err := c.Collection.Find(ctx, filter, opts...)
 	if err != nil {
 		return err
 	}
 
 	return cur.All(ctx, out)
+}
+
+func (c *Collection) QueryOne(ctx context.Context, out interface{}, filter interface{}, opts ...*options.FindOneOptions) error {
+	return c.Collection.FindOne(ctx, filter, opts...).Decode(out)
 }
